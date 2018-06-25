3adc91b5f7ae263844474a058c720fd80650ec7c
kousuutetu <kousuutetu@163.com>
2018-06-25 19:47:51 +0800

    init

---
+++ input.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Platform, TextInput } from 'react-native';
+
+export default class Input extends Component {
+  shouldComponentUpdate(nextProps){
+    return Platform.OS !== 'ios' || (this.props.value === nextProps.value &&
+      (nextProps.defaultValue == undefined || nextProps.defaultValue == '')) ||
+      (this.props.defaultValue === nextProps.defaultValue &&  (nextProps.value == undefined || nextProps.value == ''));
+  }
+
+  render() {
+    return <TextInput {...this.props} />;
+  }
+}
